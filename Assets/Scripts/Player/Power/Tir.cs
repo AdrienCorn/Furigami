005a4248e5d4d76c0bf39f1a7889b1afe6140fdc
Emma <doredia@live.fr>
2021-04-17 16:54:26 +0200

    added projectile power without photon support

---
+++ Assets/Scripts/Player/Power/Tir.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Tir : MonoBehaviour
+{
+    public GameObject proj;
+    public int velocity = 10;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            
+            if (GameObject.FindGameObjectsWithTag("projectile").Length < 3)
+            {
+                Vector3 tmpPos = this.transform.position;
+                tmpPos.x += 2;
+                GameObject tmpBall = Instantiate(proj, tmpPos, Quaternion.identity);
+                // TODO passer la ligne en dessous dans le prefab de projectile
+                tmpBall.GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3.forward * velocity); //to follow obj rotation
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/Projectile-power/Projectile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public Material active;
+    public Material inactive;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("collision");
+        if (other.tag == "interrupteur")
+        {
+            Debug.Log("activé");
+            other.GetComponent<Renderer>().material = this.active;
+
+        }
+
+        this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        StartCoroutine(ExecuteAfterTime(1));
+
+    }
+
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        // Code to execute after the delay
+        this.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        Destroy(this.gameObject, 2f);
+    }
+}
