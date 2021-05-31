6f155285fcf7cd290bda6c9696199e29d4c83944
AdrienCorn <adrien.corn@wanadoo.fr>
2021-05-31 11:33:53 +0200

    Add name selection

---
+++ Assets/Scripts/Managers/PseudoManager.cs
@@ -0,0 +1,13 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PseudoManager : MonoBehaviour
+{
+    public void PseudoChanged()
+    {
+        PhotonNetwork.LocalPlayer.NickName = GetComponent<TMP_InputField>().text;
+    }
+}
